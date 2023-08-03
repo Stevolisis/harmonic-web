ec6a9ec5da863ea1257d2e13228e839c9f91dbad
Steven Joseph <stevolisisjoseph@gmail.com>
2023-08-03 18:05:26 +0100

    tailwind no utility error fixed by fixing content path to src/

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,80 @@
+import Head from 'next/head'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-      <main>
+    <>
+        <Head>
+      <meta charSet="UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
+      <title>Introducing 😍 HarmonicWeb: The Ultimate Blogging Platform for Success! 🚀</title>
+      <meta name="description" content="Are you a passionate blogger seeking to take your blogging journey to new heights? Look no further! Welcome to HarmonicWeb, a customizable and feature-rich blogging platform designed to empower bloggers like you. With a user-friendly interface, SEO standards, and a plethora of powerful tools, HarmonicWeb is the ultimate solution to all your blogging needs. 🌟"/>
+      <meta name="keywords" content="blogging platform, blog, bloggers, customizable, feature-rich, user-friendly, SEO, technology blog, tech news, updates, insights"/>
+      <link rel="icon" href="/logo.png"/>
+      <meta name="theme-color" content="#177C65"/>
+      <meta property="og:title" content="Introducing 😍 HarmonicWeb: The Ultimate Blogging Platform for Success! 🚀"/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content="https://harmonic-web.vercel.app"/>
+      <meta property="og:image" content="/logo.png"/>
+      <meta property="og:description" content="Are you a passionate blogger seeking to take your blogging journey to new heights? Look no further! Welcome to HarmonicWeb, a customizable and feature-rich blogging platform designed to empower bloggers like you. With a user-friendly interface, SEO standards, and a plethora of powerful tools, HarmonicWeb is the ultimate solution to all your blogging needs. 🌟"/>
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta name="twitter:title" content="Introducing 😍 HarmonicWeb: The Ultimate Blogging Platform for Success! 🚀"/>
+      <meta name="twitter:image" content="/logo.png"/>
+      <meta name="twitter:description" content="Are you a passionate blogger seeking to take your blogging journey to new heights? Look no further! Welcome to HarmonicWeb, a customizable and feature-rich blogging platform designed to empower bloggers like you. With a user-friendly interface, SEO standards, and a plethora of powerful tools, HarmonicWeb is the ultimate solution to all your blogging needs. 🌟"/>
+  </Head>
+
+
+
+    <main className='py-10 px-7 md:px-32 bg-communitywave bg-bgLight'>
         <header>
-          <h1>Introducing 😍 [Your Blog Name]: The Ultimate Blogging Platform for Success! 🚀</h1>
-          <p>Are you a passionate blogger seeking to take your blogging journey to new heights? Look no further! Welcome to [Your Blog Name], a customizable and feature-rich blogging platform designed to empower bloggers like you. With a user-friendly interface, SEO standards, and a plethora of powerful tools, [Your Blog Name] is the ultimate solution to all your blogging needs. 🌟</p>
-          <p>How [Your Blog Name] Can Transform Your Blogging Experience:</p>
+          <h1 className='text-4xl font-ArchivoExtraBold'>Introducing 😍 HarmonicWeb: The Ultimate Blogging Platform for Success! 🚀</h1>
+          <p className='py-3'>Are you a passionate blogger seeking to take your blogging journey to new heights? Look no further! Welcome to HarmonicWeb, a customizable and feature-rich blogging platform designed to empower bloggers like you. With a user-friendly interface, SEO standards, and a plethora of powerful tools, HarmonicWeb is the ultimate solution to all your blogging needs. 🌟</p>
+          
+          
+          <h3 className='text-2xl font-ArchivoSemiBold mt-5'>How HarmonicWeb Can Transform Your Blogging Experience:</h3>
+          
           <ul>
-              <li>Tailored Blogging Experience: At [Your Blog Name], we understand that every blogger is unique, and so should be their blogging platform. Our fully customizable blog allows you to create a blog that aligns perfectly with your personality and niche. From selecting your blog's layout to choosing the color schemes and fonts, the possibilities are limitless! 🎨</li>
-              <li>Seamless Content Organization: Never lose track of your articles again! With [Your Blog Name]'s intuitive categories and articles section, you can effortlessly organize and manage your content. Your readers will thank you for the easy navigation and smooth user experience. 📚</li>
-              <li>Engage with Your Audience: Encourage meaningful interactions with your readers through our built-in comment features. Connect with your audience, answer their queries, and build a strong community around your blog. 💬</li>
-              <li>Newsletter Subscription: Boost your readership with our newsletter subscription feature. Allow your readers to register for your newsletter, ensuring they never miss any of your latest updates and valuable content. 💌</li>
-              <li>Stunning UI and Responsiveness: Impress your readers with a visually appealing and responsive blog. Our carefully designed user interface ensures that your blog looks stunning on all devices, enhancing user experience and reader retention. 🌈</li>
-              <li>Enhanced SEO Performance: Achieve higher search engine rankings with [Your Blog Name]'s SEO standards. Your blog will be optimized for better visibility, attracting a wider audience and increasing organic traffic. 📈</li>
-              <li>Powerful Analytics and Insights: Empower yourself with data-driven decisions using our comprehensive analytics section. Monitor the number of views, likes, and comments in a graphical layout, gaining valuable insights into your blog's performance. 📊</li>
-              <li>Seamless Integration with Google Tools: Boost your blog's online presence with Google Analytics and Google AdSense integration. Analyze user behavior, track your blog's performance, and even monetize your content with ease. 💼</li>
-              <li>Speed and Efficiency: Experience lightning-fast loading times and performance, thanks to our Next.js SSR technology. Your blog will impress readers with its speed and responsiveness. ⚡</li>
-              <li>Effortless Blog Management: Take full control of your blog with our admin features. Easily add, edit, or delete articles, categories, subscribers, staff, and users, making blog management a breeze. 🛠️</li>
-              <li>Enhanced Email Support: Build a strong connection with your readers through our email support features. Send welcome emails, notifications for new articles, and personalized messages to your subscribers. Keep your audience engaged and informed about the latest updates. 📧</li>
-              <li>Customize Your Brand: Personalize your blog's logo, name, privacy policy, "About Us" section, and social media links using our easy-to-use settings. Showcase your brand's identity and establish a unique online presence. 🎯</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Tailored Blogging Experience:</span> At HarmonicWeb, we understand that every blogger is unique, and so should be their blogging platform. Our fully customizable blog allows you to create a blog that aligns perfectly with your personality and niche. From selecting your blog's layout to choosing the color schemes and fonts, the possibilities are limitless! 🎨</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Seamless Content Organization:</span> Never lose track of your articles again! With HarmonicWeb's intuitive categories and articles section, you can effortlessly organize and manage your content. Your readers will thank you for the easy navigation and smooth user experience. 📚</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Engage with Your Audience:</span> Encourage meaningful interactions with your readers through our built-in comment features. Connect with your audience, answer their queries, and build a strong community around your blog. 💬</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Newsletter Subscription:</span> Boost your readership with our newsletter subscription feature. Allow your readers to register for your newsletter, ensuring they never miss any of your latest updates and valuable content. 💌</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Stunning UI and Responsiveness:</span> Impress your readers with a visually appealing and responsive blog. Our carefully designed user interface ensures that your blog looks stunning on all devices, enhancing user experience and reader retention. 🌈</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Enhanced SEO Performance:</span> Achieve higher search engine rankings with HarmonicWeb's SEO standards. Your blog will be optimized for better visibility, attracting a wider audience and increasing organic traffic. 📈</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Powerful Analytics and Insights:</span> Empower yourself with data-driven decisions using our comprehensive analytics section. Monitor the number of views, likes, and comments in a graphical layout, gaining valuable insights into your blog's performance. 📊</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Seamless Integration with Google Tools:</span> Boost your blog's online presence with Google Analytics and Google AdSense integration. Analyze user behavior, track your blog's performance, and even monetize your content with ease. 💼</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Speed and Efficiency:</span> Experience lightning-fast loading times and performance, thanks to our Next.js SSR technology. Your blog will impress readers with its speed and responsiveness. ⚡</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Effortless Blog Management:</span> Take full control of your blog with our admin features. Easily add, edit, or delete articles, categories, subscribers, staff, and users, making blog management a breeze. 🛠️</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Enhanced Email Support:</span> Build a strong connection with your readers through our email support features. Send welcome emails, notifications for new articles, and personalized messages to your subscribers. Keep your audience engaged and informed about the latest updates. 📧</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Customize Your Brand:</span> Personalize your blog's logo, name, privacy policy, "About Us" section, and social media links using our easy-to-use settings. Showcase your brand's identity and establish a unique online presence. 🎯</li>
           </ul>
-          <p>Your Blog, Your Success Story! [Your Blog Name] is not just a blogging platform; it's a catalyst for your blogging success. From helping you connect with your readers to providing you with the tools for growth, we are dedicated to your journey as a successful blogger. 🚀</p>
-          <p>Unleash Your Blog's Potential Today! With [Your Blog Name], you can launch your dream blog in just 5 days! Our dedicated team ensures a smooth and efficient setup, and our customizable features allow you to grow and evolve your blog effortlessly. 🌟</p>
-          <p>Join the Blogging Revolution! Be a part of the blogging revolution with [Your Blog Name]. Unlock the true potential of your blogging passion and captivate the world with your unique voice. 😍</p>
+          
+          <p>Your Blog, Your Success Story! HarmonicWeb is not just a blogging platform; it's a catalyst for your blogging success. From helping you connect with your readers to providing you with the tools for growth, we are dedicated to your journey as a successful blogger. 🚀</p>
+          <p>Unleash Your Blog's Potential Today! With HarmonicWeb, you can launch your dream blog in just 5 days! Our dedicated team ensures a smooth and efficient setup, and our customizable features allow you to grow and evolve your blog effortlessly. 🌟</p>
+          <p>Join the Blogging Revolution! Be a part of the blogging revolution with HarmonicWeb. Unlock the true potential of your blogging passion and captivate the world with your unique voice. 😍</p>
           <button>Get Started Now</button>
       </header>
       
-      <section>
-          <h2>Admin Features:</h2>
+      <section className='mt-12'>
+          <h2 className=' font-ArchivoBold text-2xl'>Admin Features:</h2>
           <ul>
-              <li>Article Management: The admin dashboard empowers you to oversee and manage all the articles published on your blog. Easily view, edit, or delete articles as per your requirements. Stay in control of your blog's content and ensure that only high-quality and relevant articles are published. 📝</li>
-              <li>Category Management: Keep your blog well-organized with the category management feature. Add, edit, or remove categories to ensure that your readers can easily find the content they are interested in. A structured and well-categorized blog enhances user experience and engagement. 🗂️</li>
-              <li>Comment Management: Engage with your readers effectively by managing comments through the admin panel. View and moderate comments to ensure a healthy and respectful discussion on your blog. Respond promptly to feedback and foster a sense of community among your readers. 💬</li>
-              <li>Subscriber Management: The admin dashboard allows you to oversee your blog's subscriber list. Keep track of new subscribers, manage existing ones, and maintain a database of engaged users. Utilize this feature to build a loyal readership and communicate with your subscribers effectively. 📈</li>
-              <li>Staff Management: If you have a team of contributors or editors, the admin dashboard facilitates staff management. Add, edit, or remove staff members with different roles and permissions. Ensure seamless collaboration among team members to enhance productivity. 👥</li>
-              <li>User Management: Administer the user base of your blog effortlessly. Monitor user accounts, roles, and activities on the platform. Tailor user permissions and ensure that your blog's community is well-managed and secure. 🔒</li>
-              <li>Analytics and Insights: Empower yourself with valuable data through the analytics section. Access key performance metrics such as the number of views, likes, articles, categories, subscribers, staff, and users. Use graphical representations to gain insights into your blog's growth and audience engagement. 📊</li>
-              <li>Email Support Features: Strengthen your bond with subscribers through personalized email support. Send welcome emails to new subscribers, notify them about new articles, and deliver important messages. Keep your audience engaged and informed about the latest updates. 💌</li>
-              <li>Site Settings Management: Customize and personalize your blog's appearance and information with ease. Use the settings section to edit your site's logo, name, privacy policy, "About Us" section, and social media links. Reflect your brand's identity and values to leave a lasting impression on your readers. ⚙️</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Article Management:</span> The admin dashboard empowers you to oversee and manage all the articles published on your blog. Easily view, edit, or delete articles as per your requirements. Stay in control of your blog's content and ensure that only high-quality and relevant articles are published. 📝</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Category Management:</span> Keep your blog well-organized with the category management feature. Add, edit, or remove categories to ensure that your readers can easily find the content they are interested in. A structured and well-categorized blog enhances user experience and engagement. 🗂️</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Comment Management:</span> Engage with your readers effectively by managing comments through the admin panel. View and moderate comments to ensure a healthy and respectful discussion on your blog. Respond promptly to feedback and foster a sense of community among your readers. 💬</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Subscriber Management:</span> The admin dashboard allows you to oversee your blog's subscriber list. Keep track of new subscribers, manage existing ones, and maintain a database of engaged users. Utilize this feature to build a loyal readership and communicate with your subscribers effectively. 📈</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Staff Management:</span> If you have a team of contributors or editors, the admin dashboard facilitates staff management. Add, edit, or remove staff members with different roles and permissions. Ensure seamless collaboration among team members to enhance productivity. 👥</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>User Management:</span> Administer the user base of your blog effortlessly. Monitor user accounts, roles, and activities on the platform. Tailor user permissions and ensure that your blog's community is well-managed and secure. 🔒</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Analytics and Insights:</span> Empower yourself with valuable data through the analytics section. Access key performance metrics such as the number of views, likes, articles, categories, subscribers, staff, and users. Use graphical representations to gain insights into your blog's growth and audience engagement. 📊</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Email Support Features:</span> Strengthen your bond with subscribers through personalized email support. Send welcome emails to new subscribers, notify them about new articles, and deliver important messages. Keep your audience engaged and informed about the latest updates. 💌</li>
+              <li className='py-3'><span className='font-ArchivoSemiBold'>Site Settings Management:</span> Customize and personalize your blog's appearance and information with ease. Use the settings section to edit your site's logo, name, privacy policy, "About Us" section, and social media links. Reflect your brand's identity and values to leave a lasting impression on your readers. ⚙️</li>
           </ul>
-          <p>With these powerful admin features, you can effectively manage your blog, interact with your audience, and optimize your blog's performance. Take control of your blogging journey and make the most out of [Your Blog Name] - the ultimate platform for your success. 🚀</p>
+          <p>With these powerful admin features, you can effectively manage your blog, interact with your audience, and optimize your blog's performance. Take control of your blogging journey and make the most out of HarmonicWeb - the ultimate platform for your success. 🚀</p>
       </section>
 
       <footer>
-          <p>© 2023 [Your Blog Name]. All rights reserved.</p>
+          <p>© 2023 HarmonicWeb. All rights reserved.</p>
       </footer>
     </main>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -27,7 +27,10 @@ module.exports = {
       },
       screens:{
         md:'800px'
-      }
+      },
+      backgroundImage: {
+        'communitywave': "url('/communitywave.png')",
+      },
     },
   },
   plugins: [],
