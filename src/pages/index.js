9c421007cf398c2c58858f0a6feb336e4a63991a
Steven Joseph <stevolisisjoseph@gmail.com>
2023-08-03 18:21:46 +0100

    general color and head theme set

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <meta name="description" content="Are you a passionate blogger seeking to take your blogging journey to new heights? Look no further! Welcome to HarmonicWeb, a customizable and feature-rich blogging platform designed to empower bloggers like you. With a user-friendly interface, SEO standards, and a plethora of powerful tools, HarmonicWeb is the ultimate solution to all your blogging needs. 🌟"/>
       <meta name="keywords" content="blogging platform, blog, bloggers, customizable, feature-rich, user-friendly, SEO, technology blog, tech news, updates, insights"/>
       <link rel="icon" href="/logo.png"/>
-      <meta name="theme-color" content="#177C65"/>
+      <meta name="theme-color" content="#073c50"/>
       <meta property="og:title" content="Introducing 😍 HarmonicWeb: The Ultimate Blogging Platform for Success! 🚀"/>
       <meta property="og:type" content="website"/>
       <meta property="og:url" content="https://harmonic-web.vercel.app"/>
